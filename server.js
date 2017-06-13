be87a9c5b42cf7416eff29986e73624e6645c97b
Antoni Angga <antoniangga14@gmail.com>
2017-06-13 16:52:07 +0700

    Sudah Selesai

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require("express");
 var app = express();
+var getPallete = require("./lib/get_pallete")
 
 app.set('view engine', 'jade');
 
 app.get("/", function (req, res) {
-  res.render("index", { palette: ["#69D2E7", "#E0E4CC", "#F38630"] });
+  res.render("index", getPallete() );
 });
 
 console.log("Server runing on http://localhost:9000")
