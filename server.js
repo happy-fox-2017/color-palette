6391346441d9b22f3071dae1573b0d8010f1aa28
myyusuf <myyusuf1911@gmail.com>
2017-06-14 07:06:39 +0700

    Release 0

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express = require("express");
 var app = express();
 
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
+const getPallette = () => {
+  return ["#69D2E7", "#E0E4CC", "#F38630"];
+}
 
 app.get("/", function (req, res) {
-  res.render("index", { palette: ["#69D2E7", "#E0E4CC", "#F38630"] });
+  res.render("index", { palettes: getPallette(), title: 'Color Pallete' });
 });
 
 console.log("Server runing on http://localhost:9000")
